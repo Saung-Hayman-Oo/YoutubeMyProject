d52c4719fb4db36e561e7ce567186564716135e0
Hayman <hayman.yoonhanthar@gmail.com>
2022-05-31 11:55:10 +0630

    my project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from "./Navbar";
+import { BrowserRouter as Router,Route,Switch } from 'react-router-dom'
+import Shop from "./LinkPage/Shop";
+import ShopDetails from "./ShopDetails";
+import NewItem from "./NewItem";
+import Shop1 from "./LinkPage/Shop1";
+import ShopDetails1 from "./ShopDetails1";
+import Login from "./LoginFloder/login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <div className="Content">
+          <Switch>
+            
+            <Route exact path="/shop">
+              <Shop1 />
+            </Route>
+            <Route path="/shop/:id">
+              {/* <ShopDetails /> */}
+              <ShopDetails1 />
+            </Route>
+            <Route path="/new-item">
+              <NewItem />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
