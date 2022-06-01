22da7e31ffaee8391999362539c82d09a6b83684
Hayman <hayman.yoonhanthar@gmail.com>
2022-06-01 10:11:13 +0630

    dojo-blog

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from "react";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+      <Navbar />
+      <div className='content'>
+        {/* {user && 
+          <Home userName = {user.name} email={user.email} phoneNumber={user.phNo} />
+        }
+
+        {!user && <Home />}
+
+        {user && <Home /> || <Navbar />}
+
+        {user ? <Home /> : <Navbar /> } */}
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/blogs/:id">
+            <BlogDetails />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
     </div>
+    </Router>
   );
 }
 
